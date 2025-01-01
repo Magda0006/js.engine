2cb1a10dbeda3fee5c5d9ecd93dcdaa9e1871fa4
Magda0006 <lenamagdamaria@gmail.com>
2025-01-01 21:19:46 +0000

    made the search index return data
    the search engine will now change the displayed city and show the current temperature of the city.

---
+++ index.js
@@ -0,0 +1,48 @@
+function changeData(response) {
+  let currentTemp = document.querySelector("#current-temperature");
+  currentTemp.innerHTML = Math.round(response.data.temperature.current);
+}
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+  let apiKey = "2td2983oa81aa7bb308858f488f7ba0c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityElement.textContent}&key=${apiKey}`;
+  axios.get(apiUrl).then(changeData);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
